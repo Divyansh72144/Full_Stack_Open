de51d8d783433d18a21ee2bc1cbfdeea0bbca210
divyanshhaagahelia <bgu298@myy.haaga-helia.fi>
2024-02-06 22:04:15 +0200

    3.22

--- Part3/index.js
+++ Part3/index.js
@@ -1,23 +1,24 @@
-const http = require("http"); //Imports Node's built-in web server
-const express = require("express");
-const app = express();
-const path = require("path");
-const morgan = require("morgan"); //Middleware are functions that can be used for handling request and response objects.s
-app.use(express.static("build"));
-const cors = require("cors");
-app.use(express.static("dist"));
-app.use(cors());
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
 
-const Person = require("./models/person");
+const app = express();
+const path = require('path');
+
+app.use(express.static('build'));
+const cors = require('cors');
+
+app.use(express.static('dist'));
+app.use(cors());
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const Person = require('./models/person');
 
 const password = process.env.DB_PASSWORD;
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
 
 const url = `mongodb+srv://Divyansh:${password}@divyansh.jvjpnhx.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
@@ -29,38 +30,42 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     validate: {
-      validator: function (v) {
+      validator(v) {
         return /^\d{2,3}-\d+$/.test(v); // Update the regex pattern
       },
-      message: (props) =>
-        `${props.value} is not a valid phone number! Please use the format xx-xxxxxxx or xxx-xxxxxxx.`,
+      message: (props) => `${props.value} is not a valid phone number! Please use the format xx-xxxxxxx or xxx-xxxxxxx.`,
     },
     required: true,
   },
 });
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
     returnedObject.id = returnedObject._id.toString();
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
     delete returnedObject._id;
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
     delete returnedObject.__v;
   },
 });
 
+// eslint-disable-next-line consistent-return
 const errorHandler = (error, request, response, next) => {
+  // eslint-disable-next-line no-console
   console.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
     if (
-      error.errors &&
-      error.errors.name &&
-      error.errors.name.kind === "minlength"
+      error.errors
+      && error.errors.name
+      && error.errors.name.kind === 'minlength'
     ) {
       return response
         .status(400)
-        .json({ error: "Name must be at least 3 characters long" });
+        .json({ error: 'Name must be at least 3 characters long' });
     }
     return response.status(400).json({ error: error.message });
   }
@@ -70,74 +75,32 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler);
 
-// const Persons = mongoose.model("Person", personSchema);
-// morgan.token("postData", (req) => {
-//   return JSON.stringify(req.body);
-// });
-
-// app.use(
-//   morgan(
-//     ":method :url :status :res[content-length] - :response-time ms :postData"
-//   )
-// );
-
 app.use(express.json());
 
-const generateRandomId = () => {
-  const min = 1;
-  const max = 1000000;
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-let people = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
-    const body = request.body;
-    console.log(typeof body.number, "this is the type");
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/info", (request, response, next) => {
+app.get('/info', (request, response, next) => {
   const currentDate = new Date();
   Person.find({})
     .then((items) => {
       response.send(
         `<p>Phonebook has info for ${items.length} persons</p>
        <br>
-       <p>${currentDate}</p>`
+       <p>${currentDate}</p>`,
       );
     })
     .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -147,52 +110,55 @@ app.get("/api/persons/:id", (request, response, next) => {
       }
     })
     .catch((error) => {
+      // eslint-disable-next-line no-console
       console.log(error);
-      response.status(400).send({ error: "malformatted id" });
+      response.status(400).send({ error: 'malformatted id' });
     })
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+// eslint-disable-next-line consistent-return
+app.post('/api/persons', (request, response, next) => {
+  const { body } = request;
   if (!body.name || body.name.length < 3 || !body.number) {
-    console.log("Validation error:", body.name);
+    // eslint-disable-next-line no-console
+    console.log('Validation error:', body.name);
     return response.status(400).json({
       error:
-        "Name must be at least 3 characters long and number must be provided",
+        'Name must be at least 3 characters long and number must be provided',
     });
   }
 
   if (!/^\d{2,3}-\d+$/.test(body.number)) {
     return response.status(400).json({
       error:
-        "Invalid phone number format! Please use the format xx-xxxxxxx or xxx-xxxxxxx.",
+        'Invalid phone number format! Please use the format xx-xxxxxxx or xxx-xxxxxxx.',
     });
-  } else {
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    });
-
-    person
-      .save()
-      .then((savedPerson) => {
-        response.json(savedPerson);
-      })
-      .catch((error) => next(error));
   }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+// eslint-disable-next-line consistent-return
+app.put('/api/persons/:id', (request, response, next) => {
+  const { body } = request;
 
   const person = {
     name: body.name,
@@ -202,25 +168,26 @@ app.put("/api/persons/:id", (request, response, next) => {
   if (!/^\d{2,3}-\d+$/.test(person.number)) {
     return response.status(400).json({
       error:
-        "Invalid phone number format! Please use the format xx-xxxxxxx or xxx-xxxxxxx.",
+        'Invalid phone number format! Please use the format xx-xxxxxxx or xxx-xxxxxxx.',
     });
   }
 
   Person.findByIdAndUpdate(request.params.id, person, {
     new: true,
     runValidators: true,
-    context: "query",
+    context: 'query',
   })
     .then((updatedPerson) => {
       if (updatedPerson) {
         response.json(updatedPerson);
       } else {
-        response.status(404).json({ error: "Person not found" });
+        response.status(404).json({ error: 'Person not found' });
       }
     })
     .catch((error) => next(error));
 });
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT);
+// eslint-disable-next-line no-console
 console.log(`Server running on port ${PORT}`);
--- Part3/mongo.js
+++ Part3/mongo.js
@@ -1,7 +1,8 @@
-const mongoose = require("mongoose");
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
+  console.log('give password as argument');
   process.exit(1);
 }
 
@@ -9,11 +10,10 @@ const password = encodeURIComponent(process.argv[2]);
 const name = process.argv[3];
 const number = process.argv[4];
 
-const dbName = "phonebook";
-const collectionName = "people";
+const dbName = 'phonebook';
 
 const url = `mongodb+srv://Divyansh:${password}@divyansh.jvjpnhx.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const PhoneSchema = new mongoose.Schema({
@@ -21,14 +21,14 @@ const PhoneSchema = new mongoose.Schema({
   number: String,
 });
 
-const Person = mongoose.model("Person", PhoneSchema);
+const Person = mongoose.model('Person', PhoneSchema);
 
 if (name && number) {
   const person = new Person({
-    name: name,
-    number: number,
+    name,
+    number,
   });
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(`added ${person.name} ${person.number} to phonebook`);
     mongoose.connection.close();
   });
